f3bb5acac266426edd95e6b31f33361dfbb2daf3
mkilian <kilian.moun@ynov.com>
2023-09-23 00:15:33 +0200

    add horizontal.go which find all of the word in the array in horizontal and update of solver.go wich call the horizontal.go function and print the return

--- solver.go
+++ solver.go
@@ -3,14 +3,26 @@ package Correction
 import "github.com/01-edu/z01"
 
 func Solver(field [10][10]rune) {
+	/// Cherche tout les mots horizontalement
+	resultHorizontal := []string{}
 	for _, i := range field {
-		z01.PrintRune('[')
-		word := Vertical(i)
-		for _, j := range word {
-			z01.PrintRune(j)
+		word := Horizontal(i)
+		if word != "" {
+			resultHorizontal = append(resultHorizontal, word)
 		}
-		z01.PrintRune(']')
 	}
+	/// On print tout les mots qu'on a trouvé horizontalement
+	z01.PrintRune('[')
+	for _, j := range resultHorizontal {
+		for _, k := range j {
+			z01.PrintRune(k)
+		}
+		if j != resultHorizontal[len(resultHorizontal)-1] {
+			z01.PrintRune(',')
+			z01.PrintRune(' ')
+		}
+	}
+	z01.PrintRune(']')
 }
 
 /// Mots Trouvés =
